0209f4ffb182e95bb67cc231530f5fd53234f214
denaldo98 <denaldo98@gmail.com>
2020-04-13 12:53:11 +0200

    Aggiunta activity di registrazione e rimozione codice delle parti java

--- app/src/main/java/com/exam/lapi_monini_politano/LoginActivity.java
+++ app/src/main/java/com/exam/lapi_monini_politano/LoginActivity.java
@@ -11,31 +11,14 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextInputEditText textNome, textCognome, textEmail, textPassword;
-    private Button btnregistra;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        textNome = findViewById(R.id.text_nome);
-        textCognome= findViewById(R.id.text_cognome);
-        textEmail = findViewById(R.id.text_email);
-        textPassword = findViewById(R.id.text_password);
-        btnregistra = findViewById(R.id.btn_registra);
-        btnregistra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.putExtra("nome", textNome.getText().toString());
-                intent.putExtra("cognome", textCognome.getText().toString());
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        });
 
-        getSupportActionBar().setTitle(getString(R.string.registra));
 
     }
 }
--- app/src/main/java/com/exam/lapi_monini_politano/SignupActivity.java
+++ app/src/main/java/com/exam/lapi_monini_politano/SignupActivity.java
@@ -2,19 +2,13 @@ package com.exam.lapi_monini_politano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    public static final int LOGIN_REQUEST = 101;
+public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        }
+        setContentView(R.layout.activity_signup);
     }
-
+}
