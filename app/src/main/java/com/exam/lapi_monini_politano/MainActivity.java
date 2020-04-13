b27d985f7c0ae18327dcd039229dbcbb2ec39ef9
denaldo98 <denaldo98@gmail.com>
2020-04-13 12:02:38 +0200

    LoginActivity

--- app/src/main/java/com/exam/lapi_monini_politano/MainActivity.java
+++ app/src/main/java/com/exam/lapi_monini_politano/MainActivity.java
@@ -2,13 +2,32 @@ package com.exam.lapi_monini_politano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int LOGIN_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivityForResult(intent, LOGIN_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        super.onActivityResult(requestCode, resultCode, intent);
+        if(requestCode == LOGIN_REQUEST) {
+            if(resultCode == RESULT_OK){
+                String nome = intent.getExtras().getString("nome");
+                String cognome = intent.getExtras().getString("cognome");
+
+                getSupportActionBar().setTitle(nome + " " + cognome);
+            }
+        }
     }
 }
